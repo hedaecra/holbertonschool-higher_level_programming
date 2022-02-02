1e571e8f65d24b9774151a39c2df8546013c8e17
hedaecra <3883@holbertonschool.com>
2022-02-01 22:29:47 -0500

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module write file UTF8 """
+
+
+def write_file(filename="", text=""):
+    """ write to a file implementation """
+    with open(filename, "w", encoding="utf-8") as myfile:
+        myfile.write(text)
