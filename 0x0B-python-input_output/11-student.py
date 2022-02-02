5741ca9f2698119db32a92f1b472cec4e066b641
hedaecra <3883@holbertonschool.com>
2022-02-01 23:19:45 -0500

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module definition of student class """
+
+
+class Student():
+    """ Class to define a student """
+    def __init__(self, first_name, last_name, age):
+        """ init of args for class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to json implementation """
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {key: value for key, value
+                    in self.__dict__.items() if key in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for i, j in json.items():
+            self.__dict__[i] = j
