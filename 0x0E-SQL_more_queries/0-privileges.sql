0dd1e501c98332919da8194186591891ae314cb3
hedaecra <3883@holbertonschool.com>
2022-03-18 12:40:49 -0500

    My privileges!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- lists all granted privileged for one user in a database
+SHOW GRANTS FOR user_0d_1@localhost;
+-- lists all granted privileged for one user in a database
+SHOW GRANTS FOR user_0d_2@localhost;
