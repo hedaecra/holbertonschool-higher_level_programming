8b6cbe0f4b8d010c74efc3569c323ccb6315f865
hedaecra <3883@holbertonschool.com>
2022-04-05 19:59:18 -0500

    Update Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const square = require('./5-square');
+const SquareDef = require('./5-square');
 
-class Square extends square {
+class Square extends SquareDef {
   charPrint (c) {
     if (c === undefined) {
       console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
